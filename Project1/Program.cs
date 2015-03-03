3bda559be630a8658622b8169c9563f2f0ad9f57
buncicka <jelena_buncic@one.pmf.uns.ac.rs>
2015-03-03 15:44:20 +0100

    Update Program.cs

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -11,7 +11,7 @@ namespace Project1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello!");
-            Console.WriteLine("Desila se izmena!");
+            Console.WriteLine("Change 1!");
             Console.WriteLine("Izmena 2");
             Console.WriteLine("Unesi svoje ime:");
             string ime = Console.ReadLine();
